6ca12adbd342b3206c6e1cdf2d7745cf2b0ddb93
Daniela Vazquez <daniela.vazquez@gmail.com>
2017-06-05 20:52:21 -0300

    basic functionalities

--- Pirate_bartender.py
+++ Pirate_bartender.py
@@ -1,37 +1,50 @@
 questions = {
-    "strong": "Do ye like yer drinks strong?",
-    "salty": "Do ye like it with a salty tang?",
-    "bitter": "Are ye a lubber who likes it bitter?",
-    "sweet": "Would ye like a bit of sweetness with yer poison?",
-    "fruity": "Are ye one for a fruity finish?",
+  "strong": "Do ye like yer drinks strong?",
+  "salty": "Do ye like it with a salty tang?",
+  "bitter": "Are ye a lubber who likes it bitter?",
+  "sweet": "Would ye like a bit of sweetness with yer poison?",
+  "fruity": "Are ye one for a fruity finish?",
 }
 
 ingredients = {
-    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
-    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
-    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
-    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
-    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+  "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+  "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+  "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+  "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+  "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 
-def style_questioning (questions, style_choice = {}):
-    for i in questions:
-        choosing_style = input(questions[i])
-        if choosing_style == 'y' or choosing_style == 'yes':
-            style_choice[i] = True
-        else:
-            style_choice[i] = False
-    return style_choice
+def style_questioning (questions):
+  preferences = {}
+  for i in questions:
+    choosing_style = input(questions[i])
+    if choosing_style == 'y' or choosing_style == 'yes':
+      preferences[i] = True
+    else:
+      preferences[i] = False
+  return preferences
 
-style_questioning(questions)
+# preferences = style_questioning(questions)
 
-# Write a function to construct a drink
-# The function should take the preferences dictionary created in the first function as a parameter. Inside the function you should create an empty list to represent the drink. For each type of ingredient which the customer said they liked you should append a corresponding ingredient from the ingredients dictionary to the drink. Finally the function should return the drink.
-#
-# To choose an ingredient from one of the ingredient lists you can use the random.choice function. This selects a random item from a list, for example:
-#
-# import random
-#
-# beatles = ["John", "Paul", "George", "Ringo"]
-# # Print the name of a random Beatle
-# print(random.choice(beatles))
+
+
+import random
+
+def recipe(preferences):
+  drink = []
+  for i in ingredients:
+    if preferences[i] == True:
+      drink.append(ingredients[i])
+  return random.choice(drink)
+
+# recipe(preferences)
+
+
+
+
+def main(questions):
+  preferences = style_questioning(questions)
+  return recipe(preferences)
+
+if __name__ == '__main__':
+  print(main(questions))
